7aa33f82e7bff8f79358742b8a03660cc38b5ce1
Shutarou Takayama <hi.mountain110916@gmail.com>
2021-10-09 20:50:07 +0900

    通らないe問題

--- abc220/e/solve.py
+++ abc220/e/solve.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env pypy3
+from math import ceil
+
+MOD = 998244353
+N, D = map(int, input().split())
+
+ans = 0
+start = min(D, N - 1)
+end = max(1, ceil(D / 2))
+squares = [1]
+for n in range(N):
+    squares.append(squares[-1] * 2 % MOD)
+D_SQUARE = 2 ** D % MOD
+
+for d in range(start, end - 1, -1):
+    trees = squares[N - d] - 1
+    if d == D:
+        tmp = D_SQUARE * 2 % MOD
+    elif d == D // 2:
+        tmp = D_SQUARE // 2 % MOD
+    else:
+        tmp = D_SQUARE
+    tmp = (tmp * trees) % MOD
+    ans = (ans + tmp) % MOD
+print(ans % MOD)
